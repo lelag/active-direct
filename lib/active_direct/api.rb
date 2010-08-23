157483128b21d36e43ff677da3f7ad5224515ead
Le Lag <gm.lelag@gmail.com>
2010-08-23 21:14:59 +0200

    Add support for JSON formated ExtDirect API declaration if /direct_api.json is called instead of /direct_api

--- lib/active_direct/api.rb
+++ lib/active_direct/api.rb
@@ -8,11 +8,28 @@ module ActiveDirect
 
     def call(env)
       if env["PATH_INFO"].match("^#{@api_path}")
-        [200, { "Content-Type" => 'text/javascript'}, [api_config]]
+        if env["PATH_INFO"].match("^#{@api_path}.json")
+          [200, { "Content-Type" => 'text/json'}, [json_api_config]]
+        else
+          [200, { "Content-Type" => 'text/javascript'}, [api_config]]
+        end
       else
         @app.call(env)
       end
     end
+
+    def json_api_config
+      config = {
+        'descriptor'=>   'REMOTING_API',
+        'url'       =>   @router_path,
+        'type'      =>   'remoting',
+        'actions'   =>   Config.method_config,
+        'namespace' =>   'App.models',
+        'srv_env'   =>    RAILS_ENV
+      }
+      config.to_json
+    end
+ 
 
     def api_config
       config = {
